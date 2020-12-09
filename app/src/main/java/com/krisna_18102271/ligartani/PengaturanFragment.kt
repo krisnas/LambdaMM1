d74daa8f2810ec6f4bfbd8f2fefce681a0fc300e
krisnas <krisnasubarkah@gmail.com>
2020-12-09 12:50:50 +0700

    menambah fitur pengaturan, search, frame account

--- app/src/main/java/com/krisna_18102271/ligartani/PengaturanFragment.kt
+++ app/src/main/java/com/krisna_18102271/ligartani/PengaturanFragment.kt
@@ -1,35 +1,29 @@
 package com.krisna_18102271.ligartani
 
-import android.content.Intent
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_account.*
+import kotlinx.android.synthetic.main.fragment_pengaturan.*
 
-class AccountFragment : Fragment() {
+class PengaturanFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false)
+
+        return inflater.inflate(R.layout.fragment_pengaturan, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (R.layout.fragment_account)
-        val button: Button = view.findViewById(R.id.btn_aboutus)
+        val button: Button = view.findViewById(R.id.btn_simpan)
 
-        btn_aboutus.setOnClickListener {
+        btn_simpan.setOnClickListener {
             val i = activity?.supportFragmentManager?.beginTransaction()
-            i?.replace(R.id.fl_fragment, KontakFragment())
-            i?.disallowAddToBackStack()
-            i?.commit()
-        }
-        iv_setting.setOnClickListener {
-            val i = activity?.supportFragmentManager?.beginTransaction()
-            i?.replace(R.id.fl_fragment, PengaturanFragment())
+            i?.replace(R.id.fl_fragment, AccountFragment())
             i?.disallowAddToBackStack()
             i?.commit()
         }
